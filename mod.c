8f8d3f8322eba2fbeaae8708a2ca897fa228fe64
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-23 15:39:13 +0000

    mod edit

--- mod.c
+++ mod.c
@@ -13,7 +13,7 @@ void mod(stack_t **stack, unsigned int line_number)
 	(void) line_number;
 	ptr = *stack;
 	next1 = (*stack)->next;
-	res = (*stack)->n % next1->n;
+	res = next1->n % (*stack)->n;
 	next1->n = res;
 	next1->prev = NULL;
 	*stack = next1;
